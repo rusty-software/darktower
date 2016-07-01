71f90aa45365bbcceecd01615e624d507a478a52
Rusty Bentley <mycoding.alert@gmail.com>
2016-07-01 18:27:37 -0500

    adding adjacent constraint

--- src/darktower/server/game.clj
+++ src/darktower/server/game.clj
@@ -1,5 +1,6 @@
 (ns darktower.server.game
-  (:require [taoensso.timbre :as log]))
+  (:require [taoensso.timbre :as log]
+            [darktower.server.board :as board]))
 
 (defn initialize-player [player]
   (assoc player :current-territory {:kingdom (:kingdom player) :row 5 :idx 3}
@@ -13,17 +14,25 @@
      :player-order (vec (map :uid init-players))
      :current-player (:uid (first init-players))}))
 
-(defn move [player {:keys [kingdom row idx type]}]
-  (let [destination (cond-> {:kingdom kingdom}
+(defn normalized-territory [{:keys [kingdom row idx type]}]
+  (cond-> {:kingdom kingdom}
 
-                            (not (nil? row))
-                            (assoc :row row)
+    (not (nil? row))
+    (assoc :row row)
 
-                            (not (nil? idx))
-                            (assoc :idx idx)
+    (not (nil? idx))
+    (assoc :idx idx)
 
-                            (not (nil? type))
-                            (assoc :type type))
-        uplayer (assoc player :move-result :moved :current-territory destination)]
+    (not (nil? type))
+    (assoc :type type)))
+
+(defn move [player destination]
+  (let [current-territory (:current-territory player)
+        neighbors (board/neighbors-for current-territory)
+        result (if (some #{destination} neighbors)
+                 {:move-result :moved :current-territory (normalized-territory destination)}
+                 {:move-result :invalid :reason "Destination must be adjacent to your current territory!" :current-territory current-territory})
+
+        uplayer (merge player result)]
     (log/info "uplayer" uplayer)
     uplayer))
