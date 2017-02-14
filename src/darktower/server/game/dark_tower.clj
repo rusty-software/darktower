6256b2d66fb9e1b6860ba52e1f3f5757106739f0
Rusty Bentley <mycoding.alert@gmail.com>
2017-02-14 13:11:38 -0600

    adding next-key

--- src/darktower/server/game/dark_tower.clj
+++ src/darktower/server/game/dark_tower.clj
@@ -1,1 +1,7 @@
 (ns darktower.server.game.dark-tower)
+
+(defn next-key [keys current-key]
+  (let [current-item-idx (.indexOf keys current-key)]
+    (if (= current-item-idx (dec (count keys)))
+      (get keys 0)
+      (get keys (inc current-item-idx)))))
--- test/darktower/server/game/dark_tower_test.clj
+++ test/darktower/server/game/dark_tower_test.clj
@@ -4,7 +4,14 @@
             [darktower.server.game.dark-tower :refer :all]))
 
 (deftest next-key-test
-  (testing "rotates through all available keys"))
+  (testing "rotates through all available keys"
+    (let [keys [:brass-key :silver-key :gold-key]]
+      (is (= :silver-key (next-key keys :brass-key)))
+      (is (= :gold-key (next-key keys :silver-key)))
+      (is (= :brass-key (next-key keys :gold-key))))
+    (let [keys [:brass-key :gold-key]]
+      (is (= :gold-key (next-key keys :brass-key)))
+      (is (= :brass-key (next-key keys :gold-key))))))
 
 (deftest guess-key-test
   (testing "given a correct guess on first key, advances to second")
