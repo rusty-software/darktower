b9cfa09c4e5131bfb1d725f2754cd1e27e2acfa5
Rusty Bentley <mycoding.alert@gmail.com>
2018-01-03 11:26:48 -0600

    updates deps, adds a line count plugin

--- project.clj
+++ project.clj
@@ -5,24 +5,29 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.6.1"
-  
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.34"]
-                 [org.clojure/core.async "0.3.442"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"
                   :exclusions [org.clojure/tools.reader]]
-                 [com.taoensso/encore "2.87.0"]
-                 [com.taoensso/sente "1.11.0"]
-                 [com.taoensso/timbre "4.8.0"]
+                 [org.clojure/core.async "0.3.465"
+                  :exclusions [org.clojure/tools.reader]]
+                 [com.taoensso/encore "2.93.0"
+                  :exclusions [org.clojure/tools.reader]]
+                 [com.taoensso/sente "1.12.0"
+                  :exclusions [org.clojure/tools.reader]]
+                 [com.taoensso/timbre "4.10.0"]
                  [environ "1.1.0"]
                  [http-kit "2.2.0"]
-                 [compojure "1.5.2"]
-                 [ring "1.5.1"]
-                 [ring/ring-defaults "0.2.3"]
-                 [ring-cors "0.1.9"]
-                 [reagent "0.6.1"]
-                 [prismatic/schema "1.1.5"]]
-  
+                 [compojure "1.6.0"]
+                 [ring "1.6.3"
+                  :exclusions [ring/ring-core commons-fileupload]]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring-cors "0.1.11"]
+                 [reagent "0.7.0"]
+                 [prismatic/schema "1.1.7"]]
+
   :plugins [[lein-figwheel "0.5.2"]
+            [lein-count "1.0.8"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
 
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
